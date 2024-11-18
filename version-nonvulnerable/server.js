32298ead258c6206d43380613ea3e9daf6d010b7
Levi Loseke <157930732+CaptnLevi96@users.noreply.github.com>
2024-11-17 20:51:27 -0500

    Front-End

--- version-nonvulnerable/server.js
+++ version-nonvulnerable/server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const path = require('path');
+const validator = require('validator');
 const app = express();
+
+// Configuration EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));  // Ajoutez cette ligne
 
 // Middleware
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/' , (req, res) => {
-    res.send('Hello World');
+// Routes
+app.get('/', (req, res) => {
+    res.render('login');
 });
 
+app.get('/test', (req, res) => {
+    const { name } = req.query;
+    const safeName = validator.escape(name || '');
+    res.render('welcome', { username: safeName });
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const safeUsername = validator.escape(username || '');
+    res.render('welcome', { username: safeUsername });
+});
 
 app.listen(3001, () => {
-    console.log('Serveur démarré sur http://localhost:3001');
+    console.log('Serveur sécurisé démarré sur http://localhost:3001');
 });
--- version-vulnerable/server.js
+++ version-vulnerable/server.js
@@ -2,20 +2,34 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Middleware
+// Configuration EJS et middleware
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/' , (req, res) => {
-    res.send('Hello World!');
+app.get('/', (req, res) => {
+    res.render('login');
 });
 
-app.get('/test' , (req, res) => {
+app.get('/test', (req, res) => {
     const { name } = req.query;
-    res.send(`Hello ${name}`);
+    // Version vulnérable : pas de protection
+    res.render('welcome', { 
+        username: name,
+        password: '' // ajout d'un password vide pour le test
+    });
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    // Version vulnérable : pas de protection
+    res.render('welcome', { 
+        username: username,
+        password: password // On passe aussi le password
+    });
 });
 
 app.listen(3000, () => {
-    console.log('Serveur démarré sur http://localhost:3000');
+    console.log('Serveur vulnérable démarré sur http://localhost:3000');
 });
